190622fbf7f8fdbb36eee45f503b4d6aa8eaf6ea
Hwang In Tak <sp301415@gmail.com>
2023-02-10 14:19:37 +0900

    Python 업데이트 & File.exists? 수정

--- _plugins/katex-init.rb
+++ _plugins/katex-init.rb
@@ -9,7 +9,7 @@ File.write("_sass/_katex.scss", katex_css)
 
 # Download fonts
 CSS_PATH = "assets/css"
-Dir.mkdir(CSS_PATH + "/fonts") unless File.exists?(CSS_PATH + "/fonts")
+Dir.mkdir(CSS_PATH + "/fonts") unless File.exist?(CSS_PATH + "/fonts")
 katex_css.scan(/src:url\((.*?)\)/).each { |font|
     File.write("#{CSS_PATH}/#{font[0]}", URI.open("#{KATEX_URL}/#{font[0]}").read())
 }
@@ -17,5 +17,5 @@ katex_css.scan(/src:url\((.*?)\)/).each { |font|
 # Download katex
 katex_js = URI.open("#{KATEX_URL}/katex.min.js").read()
 JS_PATH = "_plugins"
-Dir.mkdir(JS_PATH) unless File.exists?(JS_PATH)
+Dir.mkdir(JS_PATH) unless File.exist?(JS_PATH)
 File.write("#{JS_PATH}/katex.js", katex_js)
